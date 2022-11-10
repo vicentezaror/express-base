28e4782b70b1e230cc02c794e1fecf964f6c5b1b
Vicente Zaror <login@kydor.net>
2022-11-10 20:38:28 -0300

    feat(app): build base app.js file

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express, { Router } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+// import { router } from './routes/index.js';
+// import {
+//     logErrors,
+//     ormErrorHandler,
+//     boomErrorHandler,
+//     errorHandler
+// } from './src/middlewares/error.handler';
+
+dotenv.config();
+
+const app = express();
+
+const NODE_ENV = process.env.NODE_ENV || 'dev';
+
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+
+const ALLOWED_ORIGINS =
+    process.env.ALLLOWED_ORIGINS?.split(',')
+    || ['http://localhost:3000'];
+
+const corsOptions = {
+    origin: (origin: any, callback: any) => {
+        if (!origin || ALLOWED_ORIGINS.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Origin not allowed by CORS'));
+        }
+    },
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    maxAge: 86400,
+};
+app.use(cors(corsOptions));
+
+// router(app);
+
+// app.use(logErrors);
+// app.use(ormErrorHandler);
+// app.use(boomErrorHandler);
+// app.use(errorHandler);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-import express from 'express';
+import app from './app';
+
+const NODE_ENV = process.env.NODE_ENV || 'dev';
+const HOST = NODE_ENV === 'dev' ? 'localhost' : process.env.HOST;
+const PORT = process.env.PORT || 8080;
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`[${NODE_ENV}] Server running at http://${HOST}:${PORT}`);
+    }
+);
