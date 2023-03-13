167202fee1f197ddf4cc22ad2e6c56f8ccbcd6a8
Vicente Zaror <login@kydor.net>
2023-03-13 15:21:01 -0300

    add schema validation for user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,24 @@
 const { Router } = require('express');
 
-const { getAllUsers, createUser, getUserById } = require('../controllers/user.controllers')
+const { getAllUsers, createUser, getUserById, updateUser } = require('../controllers/user.controllers')
+const validatorHandler = require('../middlewares/validator.handler')
+const { idUserSchema, createUserSchema, updateUserSchema } = require('../schemas/user.dto')
 
 const userRoutes = Router();
 
 userRoutes.get('/', getAllUsers)
-userRoutes.get('/:id', getUserById)
-userRoutes.post('/', createUser)
+userRoutes.get('/:id',
+  validatorHandler(idUserSchema, 'params'),
+  getUserById
+)
+userRoutes.post('/',
+  validatorHandler(createUserSchema, 'body'),
+  createUser
+)
+userRoutes.patch('/:id',
+  validatorHandler(idUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  updateUser
+)
 
 module.exports = userRoutes;
---
+++ src/schemas/user.dto.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+
+const id = Joi.string().uuid()
+const firstName = Joi.string().min(2).max(30)
+const lastName = Joi.string().min(2).max(30)
+const phone = Joi.string().min(8).max(15)
+const email = Joi.string().email()
+const password = Joi.string().min(6).max(30)
+const birthDay = Joi.date().less('now')
+
+const idUserSchema = Joi.object({
+  id: id.required()
+})
+
+const createUserSchema = Joi.object({
+  firstName: firstName.required(),
+  lastName: lastName.required(),
+  phone: phone.required(),
+  email: email.required(),
+  password: password.required(),
+  birthDay: birthDay.required()
+})
+
+const updateUserSchema = Joi.object({
+  firstName: firstName,
+  lastName: lastName,
+  phone: phone,
+  email: email,
+  password: password,
+  birthDay: birthDay
+}).min(1)
+
+module.exports = {
+  idUserSchema,
+  createUserSchema,
+  updateUserSchema
+}
